02187d021922678d6c89f9f311c45a2727198b94
JasonSong97 <jaegeunsong97@gmail.com>
2025-02-22 20:57:40 +0900

    게시글 목록 최적화 전략 구현 - 테스트

---
+++ service/article-read/src/test/java/kuke/board/articleread/api/ArticleReadApiTest.java
@@ -0,0 +1,21 @@
+package kuke.board.articleread.api;
+
+import kuke.board.articleread.service.response.ArticleReadResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleReadApiTest {
+
+    RestClient restClient = RestClient.create("http://localhost:9005");
+
+    @Test
+    void readTest() {
+        ArticleReadResponse response = restClient.get()
+                .uri("/v1/articles/{articleId}", 145539405443198976L)
+                .retrieve()
+                .body(ArticleReadResponse.class);
+
+        System.out.println("response = " + response);
+
+    }
+}
