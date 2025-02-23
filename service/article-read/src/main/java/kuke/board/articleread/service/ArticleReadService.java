e503b413ed3077d18069ab21a0445064455f90a7
JasonSong97 <jaegeunsong97@gmail.com>
2025-02-23 10:45:44 +0900

    게시글 목록 최적화 전략 구현 - 서비스 & 컨트롤러

--- service/article-read/src/main/java/kuke/board/articleread/service/ArticleReadService.java
+++ service/article-read/src/main/java/kuke/board/articleread/service/ArticleReadService.java
@@ -4,18 +4,24 @@ import kuke.board.articleread.client.ArticleClient;
 import kuke.board.articleread.client.CommentClient;
 import kuke.board.articleread.client.LikeClient;
 import kuke.board.articleread.client.ViewClient;
+import kuke.board.articleread.repository.ArticleIdListRepository;
 import kuke.board.articleread.repository.ArticleQueryModel;
 import kuke.board.articleread.repository.ArticleQueryModelRepository;
+import kuke.board.articleread.repository.BoardArticleCountRepository;
 import kuke.board.articleread.service.event.handler.EventHandler;
+import kuke.board.articleread.service.response.ArticleReadPageResponse;
 import kuke.board.articleread.service.response.ArticleReadResponse;
 import kuke.board.common.event.Event;
 import kuke.board.common.event.EventPayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -28,7 +34,10 @@ public class ArticleReadService {
     private final ViewClient viewClient;
     private final LikeClient likeClient;
     private final ArticleQueryModelRepository articleQueryModelRepository;
+    private final ArticleIdListRepository articleIdListRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
     private final List<EventHandler> eventHandlers;
+    private final PageableHandlerMethodArgumentResolver pageableResolver;
 
     public void handleEvent(Event<EventPayload> event) {
         for (EventHandler eventHandler : eventHandlers) {
@@ -60,5 +69,71 @@ public class ArticleReadService {
                 .ifPresent(articleQueryModel -> articleQueryModelRepository.create(articleQueryModel, Duration.ofDays(1)));
         log.info("[ArticleReadService.fetch] fetch data, articleId={}, isPresent={}", articleId, articleQueryModelOptional.isPresent());
         return articleQueryModelOptional;
+    }
+
+    public ArticleReadPageResponse readAll(Long boardId, Long page, Long pageSize) {
+        return ArticleReadPageResponse.of(
+                readAll(
+                        readAllArticleIds(boardId, page, pageSize)
+                ),
+                count(boardId)
+        );
+    }
+
+    private List<ArticleReadResponse> readAll(List<Long> articleIds) {
+        Map<Long, ArticleQueryModel> articleQueryModelMap = articleQueryModelRepository.readAll(articleIds);
+        return articleIds.stream()
+                .map(articleId -> articleQueryModelMap.containsKey(articleId) ?
+                        articleQueryModelMap.get(articleId) :
+                        fetch(articleId).orElse(null))
+                .filter(Objects::nonNull)
+                .map(articleQueryModel ->
+                        ArticleReadResponse.from(
+                                articleQueryModel,
+                                viewClient.count(articleQueryModel.getArticleId())
+                        ))
+                .toList();
+    }
+
+    private List<Long> readAllArticleIds(Long boardId, Long page, Long pageSize) {
+        List<Long> articleIds = articleIdListRepository.readAll(boardId, (page - 1) * pageSize, pageSize);
+        if (pageSize == articleIds.size()) {
+            log.info("[ArticleReadService.readAllArticleIds] return redis data.]");
+            return articleIds;
+        }
+
+        // 원본데이터에서 정보가져오기
+        log.info("[ArticleReadService.readAllArticleIds] return original data.");
+        return articleClient.readAll(boardId, page, pageSize).getArticles().stream()
+                .map(ArticleClient.ArticleResponse::getArticleId)
+                .toList();
+    }
+
+    private long count(Long boardId) {
+        Long result = boardArticleCountRepository.read(boardId);
+        if (result != null) {
+            return result;
+        }
+        long count = articleClient.count(boardId);
+        boardArticleCountRepository.createOrUpdate(boardId, count);
+        return count;
+    }
+
+    public List<ArticleReadResponse> readAllInfiniteScroll(Long boardId, Long lastArticleId, Long pageSize) {
+        return readAll(
+            readAllInfiniteScrollArticleIds(boardId, lastArticleId, pageSize)
+        );
+    }
+
+    private List<Long> readAllInfiniteScrollArticleIds(Long boardId, Long lastArticleId, Long pageSize) {
+        List<Long> articleIds = articleIdListRepository.readAllInfiniteScroll(boardId, lastArticleId, pageSize);
+        if (pageSize == articleIds.size()) {
+            log.info("[ArticleReadService.readAllInfiniteScrollArticleIds] return redis data.]");
+            return articleIds;
+        }
+        log.info("[ArticleReadService.readAllInfiniteScrollArticleIds] return original data.");
+        return articleClient.readAllInfiniteScroll(boardId, lastArticleId, pageSize).stream()
+                .map(ArticleClient.ArticleResponse::getArticleId)
+                .toList();
     }
 }
